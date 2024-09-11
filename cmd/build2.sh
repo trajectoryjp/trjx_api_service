4f5f2adbae540e66f569c2be2a588ca7942dde67
tougei <tougei324@gmail.com>
2024-09-11 15:31:17 +0900

    edit: cmd/build2.sh

--- cmd/build2.sh
+++ cmd/build2.sh
@@ -5,3 +5,4 @@ protoc --go_out=./build --go-grpc_out=./build uas/**/**/*.proto
 protoc --go_out=./build --go-grpc_out=./build trajectory/gcs_manager/protocol/v1/*.proto
 protoc --go_out=./build --go-grpc_out=./build trajectory/uss_manager/protocol/v1/*.proto
 protoc --go_out=./build --go-grpc_out=./build trajectory/type/*.proto
+protoc --go_out=./build --go-grpc_out=./build trajectory/trajectory_gcs_service/protocol/v1/*.proto
