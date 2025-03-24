ff8ac4dc576a1b3787d02dbf5b796bd3685b6af4
Mandai Yusuke <y.mandai@trajectory.jp>
2025-03-24 19:15:23 +0900

    ビルドスクリプト、手順更新

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,5 +1,20 @@
+#!/bin/zsh
 if [[ ${PWD##*/} == "cmd" ]];then
     cd ../
 fi
-buf dep update
-buf generate
+
+cd ../../../
+
+if [ -d "github.com" ]; then
+else
+    echo "target directory('github.com') does not exist"
+    exit(1)
+fi
+
+if ! command -v buf 2>&1 >/dev/null;then
+    buf dep update
+    buf generate
+else
+    protoc --go_out=. --go-grpc_out=. github.com/trajectoryjp/trjx_api_service/**/*.proto
+fi
+
