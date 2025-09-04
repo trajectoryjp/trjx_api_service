ffaa4e9bafd973e5a14b0ed8f02e20d40a313ebc
Mandai Yusuke <y.mandai@trajectory.jp>
2025-09-04 17:51:08 +0900

    added dependency for http REST API error
    protoc-gen-openapiv2/options/annotations.proto

--- cmd/build.sh
+++ cmd/build.sh
@@ -12,9 +12,17 @@ else
 fi
 
 if ! command -v buf 2>&1 >/dev/null;then
+    echo "using buf"
     buf dep update
     buf generate
 else
-    protoc --go_out=. --go-grpc_out=. github.com/trajectoryjp/trjx_api_service/**/*.proto
+    echo "using protoc"
+    # installing dependency protoc-gen-openapiv2/options/annotations.proto via go cmd
+    go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.27.2
+    protoc \
+        -I . \
+        -I $(go list -f '{{ .Dir }}' -m github.com/grpc-ecosystem/grpc-gateway/v2@v2.27.2) \
+        --go_out=. --go-grpc_out=. \
+        github.com/trajectoryjp/trjx_api_service/**/*.proto
 fi
 
