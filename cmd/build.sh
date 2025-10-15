04bff74eaa3d842165b43cb2b51bfa7a0462eb54
UmetsuKazuki <k.umetsu@trajectory.jp>
2025-10-15 19:26:28 +0900

    外部公開apiを選定

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,5 +1,28 @@
+#!/bin/zsh
 if [[ ${PWD##*/} == "cmd" ]];then
     cd ../
 fi
-buf dep update
-buf generate
+
+cd ../../../
+
+if [ -d "github.com" ]; then
+else
+    echo "target directory('github.com') does not exist"
+    exit(1)
+fi
+
+if ! command -v buf 2>&1 >/dev/null;then
+    echo "using buf"
+    buf dep update
+    buf generate
+else
+    echo "using protoc"
+    # installing dependency protoc-gen-openapiv2/options/annotations.proto via go cmd
+    go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.27.2
+    protoc \
+        -I . \
+        -I $(go list -f '{{ .Dir }}' -m github.com/grpc-ecosystem/grpc-gateway/v2@v2.27.2) \
+        --go_out=. --go-grpc_out=. \
+        github.com/trajectoryjp/trjx_api_service/**/*.proto
+fi
+
